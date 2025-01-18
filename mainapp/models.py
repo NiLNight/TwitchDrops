5e1dc99c98f155284f6e6b07464f458b485d207a
NiLNight <bkmz-2020@inbox.ru>
2025-01-18 16:19:27 +0300

    тест моделей маркетплейса

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,132 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(
+        verbose_name='Аватар',
+        upload_to='images/avatars/%Y/%m/%d',
+        default='images/avatars/default.jpg',
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+
+
+class Category(MPTTModel):
+    """
+    Модель категорий с вложенностью
+    """
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(verbose_name='URL категории', max_length=255, blank=True)
+    description = models.TextField(verbose_name='Описание категории', max_length=300)
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name='Родительская категория'
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        db_table = 'app_categories'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2)
+    category = TreeForeignKey('Category', related_name='products', on_delete=models.CASCADE)
+    thumbnail = models.ImageField(default='default.jpg',
+                                  verbose_name='Изображение товара',
+                                  blank=True,
+                                  upload_to='images/products/%Y/%m/%d/',
+                                  validators=[
+                                      FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif', 'webp'])])
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, related_name='cart', on_delete=models.CASCADE, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, related_name='wishlist', on_delete=models.CASCADE, null=True, blank=True)
+    products = models.ManyToManyField(Product)
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+    comment = models.CharField(max_length=255)
+    review_date = models.DateTimeField(auto_now_add=True)
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, related_name='ratings', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='ratings', on_delete=models.CASCADE)
+    value = models.BigIntegerField(default=True, verbose_name='Значение')
+    created_time = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField()
+
+
+class Requisites(models.Model):
+    user = models.ForeignKey(User, related_name='requisites', on_delete=models.CASCADE)
+    number = models.PositiveSmallIntegerField()
+
+
+class Delivery(models.Model):
+    user = models.ForeignKey(User, related_name='deliveries', on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, related_name='payments', on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50)
+    requisite = models.ForeignKey(Requisites, related_name='payments', on_delete=models.CASCADE)
+    address = models.ForeignKey(Delivery, related_name='payments', on_delete=models.CASCADE)
